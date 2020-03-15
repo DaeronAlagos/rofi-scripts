f5274dfa6f181d227f86d00e5f386805fa5a1829
DaeronAlagos <mybastion@gmail.com>
2020-03-15 10:21:03 +0000

    [REF] Refactor to work in Rofi script mode

--- time-warrior.sh
+++ time-warrior.sh
@@ -1,30 +1,25 @@
 #!/usr/bin/env bash
 
-if [ "$@" = "quit" ]
-then
-    exit 0
-fi
-
-if [[ "$@" = "start" ]]; then
-    # Override the previously set prompt.
-    echo -en "\x00prompt\x1fChange prompt\n"
-    query=$( (echo ) | rofi  -dmenu -p "Tag > " )
-    if [ -n "$query "]
-    then
-    	timew start "$query"
-    	echo "quit"
-    fi
-elif [[ "$@" = "stop" ]]; then
-	timew stop
-else
-    echo -en "\x00prompt\x1fTimeWarrior\n"
-    echo -en "\0urgent\x1f0,2\n"
-    echo -en "\0active\x1f1\n"
-    echo -en "\0markup-rows\x1ftrue\n"
-    echo -en "\0message\x1fCommands\n"
-
+list_commands() {
     echo "start"
     echo "stop"
-    echo "modify"
-    echo "quit"
+}
+
+COMMAND=
+
+if [[ -z "$@" ]]; then
+    list_commands
+else
+    if [[ "$@" = "start" ]]; then
+        echo -en "\x00prompt\x1fEnter Tag: \n"
+        echo -en "\0message\x1f<b>Quit</b> to exit\n"
+        echo "quit"
+        TASK=$(echo )
+    elif [[ "$@" = "stop" ]]; then
+        timew stop >/dev/null
+        exit 0
+    else
+        timew start "$@" >/dev/null
+        exit 0
+    fi
 fi
